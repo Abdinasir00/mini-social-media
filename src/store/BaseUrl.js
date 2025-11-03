179b62031dbbc03418db1d21403e7232c622d5e0
Abdinasir Bashir <Abdi67476@gmail.com>
2025-11-03 11:55:08 +0300

    updated 02

--- src/store/BaseUrl.js
+++ src/store/BaseUrl.js
@@ -1,6 +1,10 @@
 
 // export const BASE_URL = "https://connecthub-three.vercel.app"
 
-export const BASE_URL =   "/api"
+// export const BASE_URL =   "/api"
 
 
+
+export const BASE_URL = import.meta.env.PROD
+  ? "https://connecthub-three.vercel.app/api"
+  : "/api";
